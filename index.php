cecfa3ed606d10dc87f8c6111b45b441df4952a2
Michael <michael.holt@students.plymouth.ac.uk>
2020-11-09 10:50:22 +0000

    First commit with index.php and bootsrap boilerplate code.

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+
+?>
+<!doctype html>
+
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+
+    <title>Abacus</title>
+
+    <link rel="stylesheet" href="css/bootstrap.css?v=1.0">
+
+</head>
+
+<body>
+<b>Welcome</b>
+</body>
+</html>
+
